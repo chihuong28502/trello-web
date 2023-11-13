8e99e5718d976d73fe3bfae451263aaf5685db59
Chí Hưởng <>
2023-11-13 20:12:44 +0700

    set theme MUI

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import HomeIcon from '@mui/icons-material/Home';
 import { pink } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
 function App() {
 
   return (
     <>
       <div className="">Trung Quân Dev</div>
+      <Typography variant="body2" color="text.secondary">tesstasdadsa</Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
