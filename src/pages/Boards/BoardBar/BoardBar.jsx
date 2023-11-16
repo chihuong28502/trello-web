2cf00180a6f5dde187a3fdb2b4415cad5fa5967a
Chí Hưởng <>
2023-11-16 21:59:40 +0700

    set data mock

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -1,61 +1,61 @@
-import Box from '@mui/material/Box';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import Chip from '@mui/material/Chip';
-import VpnLockIcon from '@mui/icons-material/VpnLock';
-import AddToDriveIcon from '@mui/icons-material/AddToDrive';
-import BoltIcon from '@mui/icons-material/Bolt';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import Avatar from '@mui/material/Avatar';
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Tooltip from '@mui/material/Tooltip';
-import Button from '@mui/material/Button';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-
+import Box from "@mui/material/Box";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Chip from "@mui/material/Chip";
+import VpnLockIcon from "@mui/icons-material/VpnLock";
+import AddToDriveIcon from "@mui/icons-material/AddToDrive";
+import BoltIcon from "@mui/icons-material/Bolt";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { capitalizeFirstLetter } from "~/utils/formatters";
 const MENU_STYLES = {
-  color: 'white',
-  bgcolor: 'transparent',
-  border: 'none',
-  paddingX: '5px',
-  borderRadius: '4px',
-  '& .MuiSvgIcon-root': {
-    color: 'white',
+  color: "white",
+  bgcolor: "transparent",
+  border: "none",
+  paddingX: "5px",
+  borderRadius: "4px",
+  "& .MuiSvgIcon-root": {
+    color: "white",
   },
-  '&:hover': {
-    bgcolor: 'primary.50',
+  "&:hover": {
+    bgcolor: "primary.50",
   },
 };
 
-function BoardBar() {
+function BoardBar({ board }) {
   return (
     <Box
       sx={{
         paddingX: 2,
-        width: '100%',
+        width: "100%",
         height: (theme) => theme.trello.boardBarHeight,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
         gap: 2,
-        overflowX: 'auto',
-        '&::-webkit-scrollbar-track': {
+        overflowX: "auto",
+        "&::-webkit-scrollbar-track": {
           m: 1,
         },
         bgcolor: (theme) =>
-          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
-        borderBottom: '1px solid white',
+          theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
+        borderBottom: "1px solid white",
       }}
     >
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
         <Chip
           sx={MENU_STYLES}
           icon={<DashboardIcon />}
-          label="ChiHuong28502"
+          label={board?.title}
           clickable
         />
         <Chip
           sx={MENU_STYLES}
           icon={<VpnLockIcon />}
-          label="Public/Private Workspace"
+          label={capitalizeFirstLetter(board?.type)}
           clickable
         />
         <Chip
@@ -77,12 +77,12 @@ function BoardBar() {
           clickable
         />
       </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
         <Button
           sx={{
-            color: 'white',
-            borderColor: 'white',
-            '&:hover': { borderColor: 'white' },
+            color: "white",
+            borderColor: "white",
+            "&:hover": { borderColor: "white" },
           }}
           variant="outlined"
           startIcon={<PersonAddIcon />}
@@ -93,15 +93,15 @@ function BoardBar() {
           max={7}
           total={23}
           sx={{
-            gap: '10px',
-            '& .MuiAvatar-root': {
-              width: '34px',
-              height: '34px',
-              fontSize: '16px',
-              border: 'none',
-              color: 'white',
-              cursor: 'pointer',
-              '&:first-of-type': { bgcolor: '#a4b0de' },
+            gap: "10px",
+            "& .MuiAvatar-root": {
+              width: "34px",
+              height: "34px",
+              fontSize: "16px",
+              border: "none",
+              color: "white",
+              cursor: "pointer",
+              "&:first-of-type": { bgcolor: "#a4b0de" },
             },
           }}
         >
